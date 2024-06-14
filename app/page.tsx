f2e580c4f0eedf6bc6a0521e2a8cd14f3406c8be
mattcbarrett <mattcbarrett@gmail.com>
2024-06-13 22:24:50 -0700

    Add some CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import LoginCard from "./LoginCard";
 
 export default function Home() {
   return (
-    <></>
+    <><LoginCard /></>
   );
 }
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -8,11 +8,11 @@ const Success = (props: Props) => {
   const username = usernameCookie ? usernameCookie.value : 'Email not found'
 
   return (
-    <>
-      <div>Success!</div>
-      <div>Your username is: {username}</div>
-    </>
-    
+  <div className="flex items-center justify-center min-h-screen bg-zinc-900">
+    <div className="p-8 rounded-lg shadow-lg max-w-sm w-full text-center bg-zinc-800">
+      <p>You're logged in as {username}</p>
+    </div>
+  </div>
   )
 }
 
